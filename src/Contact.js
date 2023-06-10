3f02a718878d68ef08684d27abdcc33f7d264994
JohnFull30 <johnathanfuller0@gmail.com>
2023-06-09 22:18:45 -0400

    updating site

--- src/Contact.js
+++ src/Contact.js
@@ -6,6 +6,31 @@ function Contact() {
         <h2>Let's work together...</h2>
         <p>How do you like your code?</p>
       </section>
+      <div class="contact-links">
+        <a
+          href="https://www.linkedin.com/in/john-fuller-03044322a/"
+          target="_blank"
+          class="btn contact-details"
+          rel="noreferrer"
+        >
+          <i class="fa-brands fa-linkedin fa-2x"></i> Linkedin
+        </a>
+        <a
+          id="profile-link"
+          href="https://github.com/JohnFull30"
+          target="_blank"
+          class="btn contact-details"
+          rel="noreferrer"
+        >
+          <i class="fab fa-github fa-2x"></i> GitHub
+        </a>
+        <a href="mailto:johnathanfuller0@gmail.com" class="btn contact-details">
+          <i class="fas fa-at fa-2x"></i> johnathanfuller0@gmail.com
+        </a>
+        <a href="tel:555-555-5555" class="btn contact-details">
+          <i class="fas fa-mobile-alt fa-2x"></i> (919)-987-6632
+        </a>
+      </div>
       <ion-icon size="large" name="logo-linkedin"></ion-icon>
     </>
   );
